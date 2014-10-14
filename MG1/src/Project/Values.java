1600839ad33f49ddf03b2512ed08b560aa146a85
Juli <Juli@Juli-PC.fritz.box>
2014-10-15 01:19:19 +0200

    implemented rain, lightning

--- MG1/src/Project/Values.java
+++ MG1/src/Project/Values.java
@@ -705,107 +705,98 @@ public class Values {
 		setdefault(clocks);
 		switch (weather) {
 		case "wolkig":
-			clocks[startx+1][starty+5].setPointerDegree(180, 45);
-			clocks[startx+1][starty+6].setPointerDegree(90, 0);
-			
-			clocks[startx+2][starty+4].setPointerDegree(225, 90);
-			clocks[startx+2][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+3][starty+3].setPointerDegree(225, 45);
-			clocks[startx+3][starty+4].setPointerDegree(135, 270);
-			clocks[startx+3][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+4][starty+2].setPointerDegree(225, 90);
-			clocks[startx+4][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+5][starty+2].setPointerDegree(135, 270);
-			clocks[startx+5][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+6][starty+1].setPointerDegree(225, 45);
-			clocks[startx+6][starty+3].setPointerDegree(135, 315);
-			clocks[startx+6][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+7][starty].setPointerDegree(225, 90);
-			clocks[startx+7][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+8][starty].setPointerDegree(135, 270);
-			clocks[startx+8][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+9][starty+1].setPointerDegree(315, 135);
-			clocks[startx+9][starty+3].setPointerDegree(225, 45);
-			clocks[startx+9][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+10][starty+2].setPointerDegree(225, 90);
-			clocks[startx+10][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+11][starty+2].setPointerDegree(135, 270);
-			clocks[startx+11][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+12][starty+3].setPointerDegree(135, 315);
-			clocks[startx+12][starty+4].setPointerDegree(225, 90);
-			clocks[startx+12][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+13][starty+4].setPointerDegree(135, 270);
-			clocks[startx+13][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+14][starty+5].setPointerDegree(180, 315);
-			clocks[startx+14][starty+6].setPointerDegree(270, 0);
+			drawcloud(startx, starty, clocks);
 			break;
 		case "regen":
-			clocks[startx+1][starty+5].setPointerDegree(180, 45);
-			clocks[startx+1][starty+6].setPointerDegree(90, 0);
-			
-			clocks[startx+2][starty+4].setPointerDegree(225, 90);
-			clocks[startx+2][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+3][starty+3].setPointerDegree(225, 45);
-			clocks[startx+3][starty+4].setPointerDegree(135, 270);
-			clocks[startx+3][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+4][starty+2].setPointerDegree(225, 90);
-			clocks[startx+4][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+5][starty+2].setPointerDegree(135, 270);
-			clocks[startx+5][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+6][starty+1].setPointerDegree(225, 45);
-			clocks[startx+6][starty+3].setPointerDegree(135, 315);
-			clocks[startx+6][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+7][starty].setPointerDegree(225, 90);
-			clocks[startx+7][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+8][starty].setPointerDegree(135, 270);
-			clocks[startx+8][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+9][starty+1].setPointerDegree(315, 135);
-			clocks[startx+9][starty+3].setPointerDegree(225, 45);
-			clocks[startx+9][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+10][starty+2].setPointerDegree(225, 90);
-			clocks[startx+10][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+11][starty+2].setPointerDegree(135, 270);
-			clocks[startx+11][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+12][starty+3].setPointerDegree(135, 315);
-			clocks[startx+12][starty+4].setPointerDegree(225, 90);
-			clocks[startx+12][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+13][starty+4].setPointerDegree(135, 270);
-			clocks[startx+13][starty+6].setPointerDegree(270, 90);
-			
-			clocks[startx+14][starty+5].setPointerDegree(180, 315);
-			clocks[startx+14][starty+6].setPointerDegree(270, 0);
-			for(int i = startx+1; i < startx+14; i++)
-			{
-				clocks[i][starty+7].setPointerDegree(def, 45);
-			}
-		case "Sonnig":
+			drawcloud(startx, starty, clocks);
+			drawrain(startx, starty, clocks);
+		case "gewitter":
+			drawcloud(startx, starty, clocks);
+			drawrain(startx, starty, clocks);
+			drawlightning(startx, starty, clocks);
 			break;
 		}
 	}
+	//draws lightning
+	private void drawlightning (int startx, int starty, ClockPanel[][] clocks)
+	{
+		clocks[startx][starty+2].setPointerDegree(180, 180);
+		clocks[startx][starty+3].setPointerDegree(90, 0);
+		
+		clocks[startx+1][starty+1].setPointerDegree(90, 45);
+		clocks[startx+1][starty+2].setPointerDegree(225, 45);
+		clocks[startx+1][starty+3].setPointerDegree(270, 270);
 
+		clocks[startx+2][starty].setPointerDegree(225, 45);
+		clocks[startx+2][starty+1].setPointerDegree(225, 270);
+		
+		clocks[startx+13][starty+2].setPointerDegree(180, 180);
+		clocks[startx+13][starty+3].setPointerDegree(90, 0);
+		
+		clocks[startx+14][starty+1].setPointerDegree(90, 45);
+		clocks[startx+14][starty+2].setPointerDegree(225, 45);
+		clocks[startx+14][starty+3].setPointerDegree(270, 270);
+
+		clocks[startx+15][starty].setPointerDegree(225, 45);
+		clocks[startx+15][starty+1].setPointerDegree(225, 270);
+	}
+	//draws cloud
+	private void drawcloud (int startx, int starty, ClockPanel[][] clocks)
+	{
+		clocks[startx+1][starty+5].setPointerDegree(180, 45);
+		clocks[startx+1][starty+6].setPointerDegree(90, 0);
+		
+		clocks[startx+2][starty+4].setPointerDegree(225, 90);
+		clocks[startx+2][starty+6].setPointerDegree(270, 90);
+		
+		clocks[startx+3][starty+3].setPointerDegree(225, 45);
+		clocks[startx+3][starty+4].setPointerDegree(135, 270);
+		clocks[startx+3][starty+6].setPointerDegree(270, 90);
+		
+		clocks[startx+4][starty+2].setPointerDegree(225, 90);
+		clocks[startx+4][starty+6].setPointerDegree(270, 90);
+		
+		clocks[startx+5][starty+2].setPointerDegree(135, 270);
+		clocks[startx+5][starty+6].setPointerDegree(270, 90);
+		
+		clocks[startx+6][starty+1].setPointerDegree(225, 45);
+		clocks[startx+6][starty+3].setPointerDegree(135, 315);
+		clocks[startx+6][starty+6].setPointerDegree(270, 90);
+		
+		clocks[startx+7][starty].setPointerDegree(225, 90);
+		clocks[startx+7][starty+6].setPointerDegree(270, 90);
+		
+		clocks[startx+8][starty].setPointerDegree(135, 270);
+		clocks[startx+8][starty+6].setPointerDegree(270, 90);
+		
+		clocks[startx+9][starty+1].setPointerDegree(315, 135);
+		clocks[startx+9][starty+3].setPointerDegree(225, 45);
+		clocks[startx+9][starty+6].setPointerDegree(270, 90);
+		
+		clocks[startx+10][starty+2].setPointerDegree(225, 90);
+		clocks[startx+10][starty+6].setPointerDegree(270, 90);
+		
+		clocks[startx+11][starty+2].setPointerDegree(135, 270);
+		clocks[startx+11][starty+6].setPointerDegree(270, 90);
+		
+		clocks[startx+12][starty+3].setPointerDegree(135, 315);
+		clocks[startx+12][starty+4].setPointerDegree(225, 90);
+		clocks[startx+12][starty+6].setPointerDegree(270, 90);
+		
+		clocks[startx+13][starty+4].setPointerDegree(135, 270);
+		clocks[startx+13][starty+6].setPointerDegree(270, 90);
+		
+		clocks[startx+14][starty+5].setPointerDegree(180, 315);
+		clocks[startx+14][starty+6].setPointerDegree(270, 0);
+	}
+	//draws rain
+	private void drawrain(int startx, int starty, ClockPanel[][] clocks)
+	{
+		for(int i = startx+1; i < startx+14; i++)
+		{
+			clocks[i][starty+7].setPointerDegree(def, 45);
+		}
+	}
 	// moves pointer into position for animation1
 	public void startanimation1(int rows, int columns, ClockPanel[][] clocks) {
 		if (rows % 2 == 0 && columns % 2 == 0) {
