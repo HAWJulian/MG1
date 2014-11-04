d3c872c485aa5574df46e5d3560ef1181182c97e
Juli <Juli@141.22.65.211>
2014-11-04 14:29:54 +0100

    michael ist eine nase

--- MG1/src/Motor/MotorSteuerung.java
+++ MG1/src/Motor/MotorSteuerung.java
@@ -10,28 +10,34 @@ public class MotorSteuerung {
 	static Scanner scanner = new Scanner(System.in);
 	
 	//Sende/Empfange Arduino Daten
-    SerialPort serialPort = new SerialPort("/dev/tty.usbmodem1421");
-    try {
-        serialPort.openPort();//Open serial port
-        serialPort.setParams(9600, 8, 1, 0);//Set params.
-        System.out.println("eingabe [zahl, zahl]: ");
-        while(true) {
-            String buchstabe = scanner.nextLine();
-            serialPort.writeBytes(buchstabe.getBytes());//Write data to port
-            
-            
-            byte[] buffer = serialPort.readBytes();//Read bytes from serial port
-            if(buffer != null) {
-            	String text = new String(buffer);
-            	
-            System.out.println(text);
-            }
-        }
-        
-        //serialPort.closePort();//Close serial port
-    }
-    catch (SerialPortException ex) {
-        System.out.println(ex);
-    }
+	private void ArduinoCommunication()
+	{
+		SerialPort serialPort = new SerialPort("/dev/tty.usbmodem1421");
+		try
+		{
+			serialPort.openPort();// Open serial port
+			serialPort.setParams(9600, 8, 1, 0);// Set params.
+			System.out.println("eingabe [zahl, zahl]: ");
+			while (true)
+			{
+				String buchstabe = scanner.nextLine();
+				// Write data to port
+				serialPort.writeBytes(buchstabe.getBytes());
+				byte[] buffer = serialPort.readBytes();// Read bytes from serial
+														// port
+				if (buffer != null)
+				{
+					String text = new String(buffer);
+					System.out.println(text);
+				}
+			}
+
+			// serialPort.closePort();//Close serial port
+		}
+		catch (SerialPortException ex)
+		{
+			System.out.println(ex);
+		}
+	}
     
 }
