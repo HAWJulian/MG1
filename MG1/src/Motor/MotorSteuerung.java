5737bb0babd2d8f4ab7656397abe1360b849c10a
Michael <Michael@ws-89-89.haw-1x.haw-hamburg.de>
2014-11-04 14:23:12 +0100

    Test fuer Juliane

--- MG1/src/Motor/MotorSteuerung.java
+++ MG1/src/Motor/MotorSteuerung.java
@@ -6,68 +6,32 @@ import jssc.SerialPortException;
 import java.util.Scanner;
 
 public class MotorSteuerung {
-	
+
 	static Scanner scanner = new Scanner(System.in);
-
-	///*
-	//WRITE
-	public static void main(String[] args) {
-        SerialPort serialPort = new SerialPort("/dev/tty.usbmodem1421");
-        try {
-            serialPort.openPort();//Open serial port
-            serialPort.setParams(9600, 8, 1, 0);//Set params.
-            System.out.println("buchstabe eingeben [l/r/s]: ");
-            while(true) {
-	            String buchstabe = scanner.nextLine();
-	            serialPort.writeBytes(buchstabe.getBytes());//Write data to port
+	
+	//Sende/Empfange Arduino Daten
+    SerialPort serialPort = new SerialPort("/dev/tty.usbmodem1421");
+    try {
+        serialPort.openPort();//Open serial port
+        serialPort.setParams(9600, 8, 1, 0);//Set params.
+        System.out.println("eingabe [zahl, zahl]: ");
+        while(true) {
+            String buchstabe = scanner.nextLine();
+            serialPort.writeBytes(buchstabe.getBytes());//Write data to port
+            
+            
+            byte[] buffer = serialPort.readBytes();//Read bytes from serial port
+            if(buffer != null) {
+            	String text = new String(buffer);
+            	
+            System.out.println(text);
             }
-            //serialPort.closePort();//Close serial port
         }
-        catch (SerialPortException ex) {
-            System.out.println(ex);
-        }
+        
+        //serialPort.closePort();//Close serial port
     }
-    //*/
-	
-	
-	/*
-	//READ
-	public static void main(String[] args) {
-        SerialPort serialPort = new SerialPort("/dev/tty.usbmodem1421");
-        try {
-            serialPort.openPort();//Open serial port
-            serialPort.setParams(9600, 8, 1, 0);//Set params.
-            while (true) {
-	            byte[] buffer = serialPort.readBytes();//Read bytes from serial port
-	            if(buffer != null) {
-	            	String text = new String(buffer);
-	            	
-	            System.out.println(text);
-            }
-           }
-            //serialPort.closePort();//Close serial port
-        }
-        catch (SerialPortException ex) {
-            System.out.println(ex);
-        }
+    catch (SerialPortException ex) {
+        System.out.println(ex);
     }
-	*/
-
-	
-	/*
-	//TEST
-    public static void main(String[] args) {
-        SerialPort serialPort = new SerialPort("/dev/tty.usbmodem1421");
-        try {
-            System.out.println("Port opened: " + serialPort.openPort());
-            System.out.println("Params setted: " + serialPort.setParams(9600, 8, 1, 0));
-            System.out.println("\"Hello World!!!\" successfully writen to port: " + serialPort.writeBytes("Hello World!!!".getBytes()));
-            System.out.println("Port closed: " + serialPort.closePort());
-        }
-        catch (SerialPortException ex){
-            System.out.println(ex);
-        }
-    }
-    */
     
 }
