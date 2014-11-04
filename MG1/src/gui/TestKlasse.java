8421ddba707e07b34a8035a09ed3615779ae8eac
Patrick Hilgenstock <pahilgenstock@hotmail.de>
2014-11-04 13:58:52 +0100

    nur für julian :)

--- MG1/src/gui/TestKlasse.java
+++ MG1/src/gui/TestKlasse.java
@@ -22,12 +22,14 @@ public class TestKlasse{
 			public void run() {
 				try {
 					ClockPanel frame = new ClockPanel(3, 8, 120);
+					frame.setLocation(01, 40);
 					frame.start();
 					frame.setDisplay('S');
 					Thread.sleep(5000);
 
 					//frame2.dispatchEvent(new WindowEvent (frame2, WindowEvent.WINDOW_CLOSING));
 					ClockPanel frame2 = new ClockPanel(8 , 16 ,120);
+
 					frame2.start();
 					Presentation presi = new Presentation(frame2);
 					presi.run();
